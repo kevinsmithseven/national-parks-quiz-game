f31ec531e73db15fb8a17e7aa5e3e2987d771a4d
kevinsmithseven <kevinsmithseven@gmail.com>
2023-09-23 17:15:56 -0500

    added start page body styling and added questions in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,38 @@
-//Starting Quiz
+//Question and answer arrays
+
+var questions = {
+    question1: "Which president established the greatest number of National Parks?", 
+    question2: "What was the very first National Park established?", 
+    question3: "Which National Park were scenes from Star Wars Episode IV: A New Hope filmed, representing the fictional planet of Tatooine?",
+    question4: "The biggest single mountain on earth is found in which National Park?",
+    question5: "In which National Park can you find the world’s largest known living single stem tree, General Sherman?"
+};
+
+
+
+
+
+
+//Create variables using DOM
 var startButton = document.getElementById("start-button");
+
+
+
+//Need a function to start the game
+
+
+
+
+//Need function to run timer, reduce time if a wrong question answered
+
+
+
+
+
+
+
+
+
+
+
+//Starting Quiz
