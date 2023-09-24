65de30978e81008159f7ab37e40381a65b4b01cb
kevinsmithseven <kevinsmithseven@gmail.com>
2023-09-24 11:40:58 -0500

    added quiz questions and answers in js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-//Question and answer arrays
+//Question and answer objects
 
 var questions = {
     question1: "Which president established the greatest number of National Parks?", 
@@ -7,6 +7,50 @@ var questions = {
     question4: "The biggest single mountain on earth is found in which National Park?",
     question5: "In which National Park can you find the world’s largest known living single stem tree, General Sherman?"
 };
+
+var answersQuestion1 = {
+    question1Option1: "Jimmy Carter",
+    question1Option2: "Theodore Roosevelt",
+    question1Option3: "William Taft",
+    question1Option4: "Barack Obama"
+};
+
+var answersQuestion2 = {
+    question2Option1: "Yosemite National Park",
+    question2Option2: "Sequoia National Park",
+    question2Option3: "Yellowstone National Park",
+    question2Option4: "Mount Rainier National Park"
+};
+
+var answersQuestion3 = {
+    question3Option1: "Grand Canyon National Park",
+    question3Option2: "Bryce Canyon National Park",
+    question3Option3: "Joshua Tree National Park",
+    question3Option4: "Death Valley National Park"
+};
+
+var answersQuestion4 = {
+    question4Option1: "Yosemite National Park",
+    question4Option2: "Hawaii Volcanoes National Park",
+    question4Option3: "Rocky Mountain National Park",
+    question4Option4: "Grand Teton National Park"
+};
+
+var answersQuestion5 = {
+    question5Option1: "Redwood National Park",
+    question5Option2: "Olympic National Park",
+    question5Option3: "King's Canyon National Park",
+    question5Option4: "Sequoia National Park"
+};
+
+var correctAnswers = {
+    question1: answersQuestion1.question1Option1,
+    question2: answersQuestion2.question2Option3,
+    question3: answersQuestion3.question3Option4,
+    question4: answersQuestion4.question4Option2,
+    question5: answersQuestion5.question5Option4
+};
+
 
 
 
@@ -23,16 +67,17 @@ var startButton = document.getElementById("start-button");
 
 
 
-//Need function to run timer, reduce time if a wrong question answered
+//Need function to run timer
+
+var
+
+
+//Need function to check if answer is correct
+
+
+
+//Need function to reduce time if answer is wrong
 
 
 
 
-
-
-
-
-
-
-
-//Starting Quiz
