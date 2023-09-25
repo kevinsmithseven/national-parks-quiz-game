214a1866799b9d7e69242ea31023fa714e9fe50f
kevinsmithseven <kevinsmithseven@gmail.com>
2023-09-25 17:11:37 -0500

    added header section in html and associated css styling and started start game logic in js

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,30 @@
 //Question and answer objects
 
-var myQuestions = [{
+const myQuestions = [{
     questionText: "Which president established the greatest number of National Parks?",
     questionChoices: ["Jimmy Carter", "Theodore Roosevelt", "William Taft", "Barack Obama"],
-    correctAnswer: "Jimmy Carter"
+    correctAnswer: 0
 }, {
     questionText: "What was the very first National Park established?",
     questionChoices: ["Yosemite National Park", "Sequoia National Park", "Yellowstone National Park", "Mount Rainier National Park"],
-    correctAnswer: "Yellowstone National Park",
+    correctAnswer: 2
 }, {
     questionText: "Which National Park were scenes from Star Wars Episode IV: A New Hope filmed, representing the fictional planet of Tatooine?",
     questionChoices: ["Grand Canyon National Park", "Bryce Canyon National Park", "Joshua Tree National Park", "Death Valley National Park"],
-    correctAnswer: "Death Valley National Park",
+    correctAnswer: 3
 }, {
     questionText: "The biggest single mountain on earth is found in which National Park?",
     questionChoices: ["Yosemite National Park", "Hawaii Volcanoes National Park", "Rocky Mountain National Park", "Grand Teton National Park"],
-    correctAnswer: "Hawaii Volcanoes National Park",
+    correctAnswer: 1
 }, {
     questionText: "In which National Park can you find the world’s largest known living single stem tree, General Sherman?",
     questionChoices: ["Redwood National Park", "Olympic National Park", "King's Canyon National Park", "Sequoia National Park"],
-    correctAnswer: "Sequoia National Park"
+    correctAnswer: 3
 }]
 
-    
 
 
-
-
-    
-  
-
-
-
-
-
-
-
-
+// Add in event listeners for start
 
 //Create variables 
 var score = 0;
@@ -44,17 +32,25 @@ var wrongPenalty = 10;
 var questionNumber = 0;
 var startButton = document.getElementById("start-button");
 var countdownTimer = document.getElementById("timer");
-var startSection = document.getElementById("start")
+var startSection = document.getElementById("start-section")
+var quizTimer;
+var timerCount;
 
 
 
-
+// Start Game
+function startGame() {
+    startButton.addEventListener("click", function () {
+        countdown();
+        startSection.style.display = "none";
+    })
+}
 
 
 //Need a function to navigate to next question
 
 function navigate(direction) {
-    
+
 }
 
 
@@ -69,11 +65,10 @@ function countdown() {
 
     var quizTimer = setInterval(function () {
         // as long as "timeleft" is greater than 1
+        timeLeft--;
+        countdownTimer.textContent = timeLeft + " seconds remaining";
         if (timeLeft > 1) {
-            // set the text content of countdownTimer to show remaining seconds
-            countdownTimer.textContent = timeLeft + " seconds remaining";
-            // decrement timeLeft by 1
-            timeLeft--;
+
         } else if (timeLeft === 1) {
             // when timeLeft gets to 0, set countdownTimer to "second" instead of "seconds"
             countdownTimer.textContent = timeLeft + " second remaining";
@@ -81,7 +76,7 @@ function countdown() {
             // Once timeLeft gets to 0, set countdownTimer to an empty string
             countdownTimer.textContent = "";
             //stop timer
-            clearInterval(countdown);
+            clearInterval(quizTimer);
             //do i need a display message?
             // do i need a function to end the quiz?
             endQuiz();
